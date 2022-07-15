9da84eee836574e3227aa64cc8c656769748759e
abdulrhmanalqassas <46942535+abdulrhmanalqassas@users.noreply.github.com>
2022-07-15 19:13:03 +0200

    ESS

--- Frontend/src/components/Land.js
+++ Frontend/src/components/Land.js
@@ -22,6 +22,7 @@ export default function Land() {
           <h3> VACCINE VERIFICATION SYSTEM</h3>
           <div className="newslatter"></div>
         </div>
+        
       </div>
 
       <section className="about">
@@ -36,7 +37,7 @@ export default function Land() {
               This software system will be a web abb on top of a block chain to
               help users and governments to Check and insure the vaccine state
               of the citizens. This system will be implemented and designed to
-              ens ure the authenticity and validity of the data using the
+              ens ure the   authenticity and validity of the data using the
               blockchain techn ology and keeping in mind being user friendly and
               easy to use on any devise that use a web browser and almost
               impossible to be hacked
